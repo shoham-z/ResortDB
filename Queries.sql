31d416354076b5e53353bfebeb4a855d53181eee
Shoham Zeharya <shozeh123@gmail.com>
2022-03-09 19:56:52 +0200

    added script to create all tables

--- Queries.sql
+++ Queries.sql
@@ -1,4 +1,5 @@
 --selecting the clients that got in between 2021-11-28 and 2021-12-5
-select client_name
-from Client NATURAL JOIN Orders
-where entry_date >= '2021-11-28' and entry_date <= '2021-12-23'
+CREATE TABLE hannukah(  client_name varchar(30));
+INSERT INTO hannukah SELECT client_name
+from Customers NATURAL JOIN RoomOrder
+
