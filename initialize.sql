5b7e2697287c8cb8231e6fd8453c4b48aab84372
Shoham Zeharya <shozeh123@gmail.com>
2022-03-09 19:57:20 +0200

    added script to create all tables

--- initialize.sql
+++ initialize.sql
@@ -1,35 +1,44 @@
-drop table Room;
-drop table MaintenanceEmp;
-drop table Orders;
-drop table Client;
-
 create table if not exists Room(
-    room_id INTEGER NOT NULL PRIMARY KEY,
-    num_windows INTEGER NOT NULL,
-    max_people INTEGER NOT NULL,
-    price INTEGER NOT NULL
+    room_id INT NOT NULL PRIMARY KEY,
+    name varchar(30),
+    max_people INT NOT NULL,
+    price INT NOT NULL
 );
 
-create table if not exists Client(
-  client_id NUMERIC(9, 0) primary key,
-  client_name varchar(30)
+create table Customer(
+  client_id INT,
+  client_name varchar(30),
+  age INT,
+  isMale BIT NOT NULL,
+    constraint id_key PRIMARY KEY (client_id)
 );
 
-create table if not exists Orders(
-    client_id INTEGER,
-    room_id INTEGER,
-    order_price INTEGER,
-    entry_date DATE NOT NULL,
-    FOREIGN KEY (client_id) REFERENCES Client(client_id)
+CREATE TABLE MaintenanceEmp (
+    emp_id INT,
+    emp_name varchar(30),
+    age INT,
+    isMale BIT NOT NULL,
+    room_id INT,
+    salary INT NOT NULL,
+    constraint id_key PRIMARY KEY (emp_id),
+    CONSTRAINT FOREIGN KEY (room_id) REFERENCES Room(room_id) ON DELETE CASCADE
 );
 
+CREATE TABLE RoomOrder(
+    OrderId INT,
+    amount INT,
+    room_number INT,
+    customer_id INT,
+    constraint id PRIMARY KEY (OrderId),
+    CONSTRAINT FOREIGN KEY (room_number) REFERENCES Room(room_id) ON DELETE CASCADE,
+    CONSTRAINT FOREIGN KEY (customer_id) REFERENCES Customer(client_id) ON DELETE CASCADE
+);
 
-CREATE TABLE if not exists MaintenanceEmp (
-    emp_number INT NOT NULL,
-    emp_name VARCHAR(20) NOT NULL,
-    room_id INTEGER,
-    hire_date DATETIME,
-    salary NUMERIC(7,2) NOT NULL,
-    UNIQUE (emp_number, emp_name),
-    FOREIGN KEY (room_id) REFERENCES Room(room_id)
+CREATE TABLE Maintane(
+    room_id INT,
+    emp_id INT,
+    CONSTRAINT FOREIGN KEY (room_id)
+        REFERENCES Room(room_id)
+        ON DELETE CASCADE,
+    CONSTRAINT FOREIGN KEY (emp_id) REFERENCES MaintenanceEmp(emp_id) ON DELETE CASCADE
 );
