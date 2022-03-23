4a200c0376473911b3c689caded2bfdb395c3daf
Shoham Zeharya <shozeh123@gmail.com>
2022-03-23 18:34:38 +0200

    updated dbms

--- Queries.sql
+++ Queries.sql
@@ -1,5 +1,55 @@
---selecting the clients that got in between 2021-11-28 and 2021-12-5
-CREATE TABLE hannukah(  client_name varchar(30));
-INSERT INTO hannukah SELECT client_name
-from Customers NATURAL JOIN RoomOrder
+create table if not exists Room(
+    room_id INT NOT NULL PRIMARY KEY,
+    name varchar(30),
+    max_people INT NOT NULL,
+    price INT NOT NULL
+);
+create table Customer(
+  client_id INT,
+  client_name varchar(30),
+  age INT,
+  isMale BIT NOT NULL,
+    constraint id_key PRIMARY KEY (client_id)
+);
+CREATE TABLE MaintenanceEmp (
+    emp_id INT,
+    emp_name varchar(30),
+    age INT,
+    isMale BIT NOT NULL,
+    room_id INT,
+    salary INT NOT NULL,
+    constraint id_key PRIMARY KEY (emp_id),
+    CONSTRAINT FOREIGN KEY (room_id) REFERENCES Room(room_id) ON DELETE CASCADE
+);
+CREATE TABLE RoomOrder(
+    OrderId INT,
+    amount INT,
+    room_id INT,
+    customer_id INT,
+    constraint id PRIMARY KEY (OrderId),
+    CONSTRAINT FOREIGN KEY (room_id) REFERENCES Room(room_id) ON DELETE CASCADE,
+    CONSTRAINT FOREIGN KEY (customer_id) REFERENCES Customer(client_id) ON DELETE CASCADE
+);
+CREATE TABLE Maintane(
+    room_id INT,
+    emp_id INT,
+    CONSTRAINT FOREIGN KEY (room_id)
+        REFERENCES Room(room_id)
+        ON DELETE CASCADE,
+    CONSTRAINT FOREIGN KEY (emp_id) REFERENCES MaintenanceEmp(emp_id) ON DELETE CASCADE
+);
 
+
+CREATE TABLE oldMales(client_name varchar(30));
+INSERT INTO oldMAles SELECT client_name FROM Customer
+WHERE Customer.age > 60 and Customer.isMale = 1;
+
+CREATE TABLE emptyRooms(roomId INT);
+INSERT INTO emptyRooms SELECT room_id
+FROM Room
+WHERE NOT EXISTS (SELECT room_id FROM Room);
+
+CREATE TABLE toAssign(emp_id INT);
+INSERT INTO toAssign SELECT emp_id
+FROM MaintenanceEmp
+WHERE NOT EXISTS(SELECT emp_id FROM Maintane);
--- python_tool.py
+++ python_tool.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
-from datetime import  datetime
-import sqlite3
+from datetime import datetime
 import random
+
 
 def random_date(start, end):
     delta = end - start
@@ -10,13 +10,8 @@ def random_date(start, end):
     date_str = str(start + timedelta(seconds=random_second)).replace('-', '/')
     return datetime.strptime(date_str[:10], '%Y/%m/%d')
 
-# Connecting to sqlite
-conn = sqlite3.connect('identifier.sqlite')
-  
-# Creating a cursor object using the 
-# cursor() method
-cursor = conn.cursor()
 
+file = open('queries.sql', 'w')
   
 # Queries to INSERT records.
 for i in range(1, 101, 1):
@@ -27,8 +22,8 @@ for i in range(1, 101, 1):
         price = 2000
     else:
         price = 4500
-    command = 'INSERT INTO Room values (' + str(i) + ',' +str(num_windows) + ',' + str(max_people) + ',' + str(price) + ')'
-    cursor.execute(command)
+    command = 'INSERT INTO room values (' + str(i) + ',' +str(num_windows) + ',' + str(max_people) + ',' + str(price) + ')'
+    print(command)
 
 date1 = datetime.strptime('1/1/2019 1:30 PM', '%m/%d/%Y %I:%M %p')
 date2 = datetime.strptime('2/23/2022 4:50 AM', '%m/%d/%Y %I:%M %p')
@@ -38,18 +33,8 @@ client_name_list = ['David Joyner', 'David Zuber', 'Brenton Joyner',
              'Shelba Fry', 'Maren Fry']
 for client_name in client_name_list:
     client_id = random.randrange(2123423, 3562352, 201)
-    command = 'INSERT INTO Client values'+'(' + str(client_id) + "," + "\'" + client_name + "\'" + ')'
-    cursor.execute(command)
+    command = 'INSERT INTO customer values'+'(' + str(client_id) + "," + "\'" + client_name + "\'" + ')'
+    print(command)
 
 date = str(random_date(date1, date2))[:10]
-# Display data inserted
-print("Data Inserted in the table: ")
-data = cursor.execute('''SELECT * FROM Room''')
-for row in data:
-    print(row)
-  
-# Commit your changes in the database    
-conn.commit()
-  
-# Closing the connection
-conn.close()
+
